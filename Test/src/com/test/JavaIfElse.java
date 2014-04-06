d26420803f2c79fe42a5dda854620be83a5eaba0
shripaul <shripaul5@gmail.com>
2014-04-05 23:26:11 -0400

    Class1

---
+++ Test/src/com/test/JavaIfElse.java
@@ -0,0 +1,15 @@
+package com.test;
+
+public class JavaIfElse {
+
+	public static void main(String[] args) {
+		int i = 18;
+		if(i%2==0){
+		System.out.println("The number is even");
+		}
+		else if(i%2==1){
+System.out.println("The number is odd");
+	}
+
+}
+}
