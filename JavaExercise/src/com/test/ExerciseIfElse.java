704167c5775cf2acc6d1c0a884ae7c5d392ead03
shripaul <shripaul5@gmail.com>
2014-04-13 10:20:48 -0400

    all files

---
+++ JavaExercise/src/com/test/ExerciseIfElse.java
@@ -0,0 +1,15 @@
+package com.test;
+
+public class ExerciseIfElse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int mark = 49;
+		System.out.println("The mark is" + mark);
+		
+			
+		}
+
+	}
+
+
