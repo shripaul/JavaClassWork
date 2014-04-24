f8074ff273d862722faf81fb50de9d82849939e3
shripaul <shripaul5@gmail.com>
2014-04-24 09:54:27 -0400

    Mixmax

---
+++ BasicWebdriver/src/com/test/HomePageTest.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import org.junit.Test;
+
+public class HomePageTest extends BaseClass {
+	@Test
+	public void home(){
+		driver.findElement(By-id(u_0_1)).sendKeys("Reaz");
+		
+	}
+	
+
+}
